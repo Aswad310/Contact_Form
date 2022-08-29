12a1951bc85a5c277f8f580a1ca416e816c1142e
Aswad Ali <aswadali310@gmail.com>
2022-08-30 02:55:39 +0500

    frond-end validation done

--- index.php
+++ index.php
@@ -14,6 +14,29 @@
 
     <div class="container">
         <h1>Get in touch!</h1>
+      
+        <div id="error"></div>
+
+        <form>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                <small>We will never share your email with anyone else</small>
+            </div>
+
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" class="form-control" id="subject">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">What would you like to ask us?</label>
+                <textarea class="form-control" id="content" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
     </div>
     
 
@@ -22,5 +45,11 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+  
+    
+    <!-- Validation.js -->
+    <script src="validation.js" text="text/javascript"></script>
+
+
+</body>
 </html>
---
+++ validation.js
@@ -0,0 +1,31 @@
+// Validation
+
+// Prevents the form from submitting untill it pass from vaidations
+$("form").submit(function(e){
+    e.preventDefault();
+
+    // empty variable
+    var error = "";
+
+    if($("#email").val() == "")
+    {
+        error += "The email field is required.<br>";
+    }
+    if($("#subject").val() == "")
+    {
+        error += "The subject field is required.<br>";
+    }
+    if($("#content").val() == "")
+    {
+        error += "The content field is required.<br>";
+    }
+    if(error!="")
+    {
+        $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form</strong></p>'+ error +'</div>');
+    }
+    else
+    {
+        // after validation completed this will unbind the preventions
+        $("form").unbind("submit").submit();
+    }
+});  
